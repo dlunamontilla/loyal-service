21f0ee282c06b070ed5d10c261f2982989c12dff
David E Luna M <davidlunamontilla@gmail.com>
2020-07-15 06:05:09 -0400

    Implementación de logo y menú en la primera sección - Sección Inicio

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-console.log( "Se encuentra perfectamente vinculado" );
+const logotipo = async ( _elemento, _ruta ) => {
+  const res = await fetch("multimedia/imagen/loyal-services-logo.svg"),
+        recurso = await res.text();
+
+  console.log( recurso );
+
+  _elemento.insertAdjacentHTML( "beforeend", recurso );
+}
+
+const ruta = "multimedia/imagen/loyal-services-logo.svg",
+      logo = document.querySelector( "#logotipo" );
+
+if ( logo !== null )
+  logotipo( logo, ruta );
