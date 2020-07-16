47ac41d89342d3671bd079f4540a11f274f4d227
David E Luna M <davidlunamontilla@gmail.com>
2020-07-15 22:56:23 -0400

    Implementación de iconos en la sección de servicios

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,36 @@
 (function () {
-  const logotipo = async ( _elemento, _ruta ) => {
-    const res = await fetch("multimedia/imagen/loyal-services-logo.svg"),
-          recurso = await res.text();  
-    _elemento.insertAdjacentHTML( "beforeend", recurso );
-  }
-  
-  const ruta = "multimedia/imagen/loyal-services-logo.svg",
-        logo = document.querySelector( "#logotipo" );
-  
-  if ( logo !== null )
-    logotipo( logo, ruta );
+	const imagen = async (_elemento, _ruta) => {
+		const res = await fetch(_ruta),
+			recurso = await res.text();
+		_elemento.insertAdjacentHTML("beforeend", recurso);
+	}
 
-  // Obtener datos de la plantilla
-  const cards = document.querySelector( "#template-cards" ),
-        title = document.querySelector( "#template-title" ),
-        buttonTel = document.querySelector( "#template-button-tel" );
+	const ruta = "multimedia/imagen/loyal-services-logo.svg",
+		logo = document.querySelector("#logotipo");
 
-  // Obtener Secciones:
-  const servicesContent = document.querySelector( "#services-content" );
+	if (logo !== null)
+		imagen(logo, ruta);
+
+	// Obtener datos de la plantilla
+	const cards = document.querySelector("#template-cards"),
+		title = document.querySelector("#template-title"),
+		buttonTel = document.querySelector("#template-button-tel");
+
+	// Obtener Secciones:
+	const servicesContent = document.querySelector("#services-content");
 
 
-  // Implementar plantilla en: Services:
-  const servicesTitle = title.cloneNode( true ),
-        servicesCards = cards.cloneNode( true );
+	// Implementar plantilla en: Services:
+	const servicesTitle = title.cloneNode(true),
+		servicesCards = cards.cloneNode(true);
 
-  servicesTitle.textContent = "Services";
-  servicesContent.append( servicesTitle.content, servicesCards.content, buttonTel.cloneNode( true ).content );
-  
+	servicesTitle.textContent = "Services";
+	servicesContent.append(servicesTitle.content, servicesCards.content, buttonTel.cloneNode(true).content);
+
+	const iconos = document.querySelectorAll("[data-src]");
+
+	if ( iconos.length > 0 )
+		iconos.forEach((icono) => {
+			imagen( icono, icono.dataset.src );
+		});
 }())
