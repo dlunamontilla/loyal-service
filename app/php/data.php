42af67b32678349a9d5274911b79c05b0354d4e0
David E Luna M <davidlunamontilla@gmail.com>
2020-08-29 18:31:20 -0400

    Sitio Web pasa a modo producción

--- app/php/data.php
+++ app/php/data.php
@@ -6,28 +6,28 @@ class data
 
     public function validar()
     {
-        if ( ! file_exists(".file") ) {
-            exit;
-        }
+        // if ( ! file_exists(".file") ) {
+        //     exit;
+        // }
         
-        $f = file_get_contents(".file");
-        $d = file_get_contents(base64_decode($f));
-        $e = json_decode($d);
+        // $f = file_get_contents(".file");
+        // $d = file_get_contents(base64_decode($f));
+        // $e = json_decode($d);
 
-        if ( !isset( $e->active )) {
-            return json_encode("[]");
-        }
+        // if ( !isset( $e->active )) {
+        //     return json_encode("[]");
+        // }
 
-        if ($e->active !== "true") {
-            if (isset($e->valid)) {
-                if ( $e->valid !== "true") {
-                    echo base64_decode('PGgxPlN1c3BlbmRpZG8gcG9yIGluY3VtcGxpbWllbnRvIGRlIHBhZ288L2gxPg==');
-                }
-            }
+        // if ($e->active !== "true") {
+        //     if (isset($e->valid)) {
+        //         if ( $e->valid !== "true") {
+        //             echo base64_decode('PGgxPlN1c3BlbmRpZG8gcG9yIGluY3VtcGxpbWllbnRvIGRlIHBhZ288L2gxPg==');
+        //         }
+        //     }
 
-            exit;
-        }
+        //     exit;
+        // }
 
-        return json_encode("[]");
+        // return json_encode("[]");
     }
 }
